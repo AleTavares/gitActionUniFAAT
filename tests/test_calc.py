aed5ebc1314abe42d5871297d8f219f0509ff528
shunshunyi <davidwphk@gmail.com>
2025-09-08 20:32:30 -0300

    feat: Adiciona função de potenciação e teste

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,12 @@
 from app.calc import soma, multiplica
+from app.calc import potencia
 
 def test_soma():
     assert soma(2, 3) == 5
 
 def test_multiplica():
     assert multiplica(2, 3) == 6
+def test_potencia():
+    assert potencia(2, 3) == 8
+    assert potencia(5, 0) == 1
+    assert potencia(3, 2) == 9
