a0f0f0da139b8f87863aff8decd0c95091cd0a03
shunshunyi <davidwphk@gmail.com>
2025-09-08 20:59:56 -0300

    ci: restringe CI para main e feature/TF_04; fix: corrige teste potencia

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,10 @@
 from app.calc import soma, multiplica
+from app.calc import potencia  # Importing the potencia function
 
+def test_potencia():
+    assert potencia(2, 3) == 8  # corrigido para o valor correto
+    assert potencia(5, 0) == 1
+    assert potencia(3, 2) == 9
 def test_soma():
     assert soma(2, 3) == 5
 
