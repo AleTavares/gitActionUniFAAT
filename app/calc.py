ba7dc6266bf27b883be15bb645878e1aeab46376
Alison Medeiros <Alisonmedeirosprofissional@gmail.com>
2025-09-08 20:10:57 -0300

    Teste calculo divisao subtracao

--- app/calc.py
+++ app/calc.py
@@ -4,3 +4,12 @@ def soma(a, b):
 
 def multiplica(a, b):
     return a * b
+
+def subtracao(a, b):
+    return a - b
+
+def divisao(a, b):
+    if b == 0:
+        raise ValueError("Divisão por zero não é permitida")
+    return a / b
+
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,20 @@
-from app.calc import soma, multiplica
+from app.calc import soma, multiplica, subtracao, divisao
 
 def test_soma():
     assert soma(2, 3) == 5
 
 def test_multiplica():
     assert multiplica(2, 3) == 6
+
+def test_subtracao():
+    assert subtracao(2, 2) == 0
+
+def test_divisao():
+    assert divisao(6, 2) == 3
+
+def test_divisao_por_zero():
+    try:
+        divisao(5, 0)
+        assert False, "Deveria ter levantado ValueError"
+    except ValueError:
+        assert True
