79c4c3d3a1b1e8480d2fa80c4359eb7a4a8c43e4
unknown <bp40111@gmail.com>
2025-09-08 20:05:05 -0300

    Adicao Subtracao e Divisao

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,13 @@
-from app.calc import soma, multiplica
+from app.calc import soma, multiplica, divide, subtrai
 
 def test_soma():
     assert soma(2, 3) == 5
 
 def test_multiplica():
     assert multiplica(2, 3) == 6
+
+def test_divide():
+    assert divide(6, 2) == 3
+
+def test_subtrai():
+    assert subtrai(10, 3) == 7
