207699620b8f81401462efdc46dfa38c372d8eda
leonardofsano <161530679+leonardofsano@users.noreply.github.com>
2025-09-08 21:38:23 -0300

    Introduzindo erro proposital na função soma para demonstrar CI

--- app/calc.py
+++ app/calc.py
@@ -1,6 +1,5 @@
 def soma(a, b):
-    # return a - b  # ERRO proposital
-    return a + b  # Correção
+    return a - b  # ERRO proposital para demonstrar CI
 
 def subtracao(a, b):
     return a - b
