aaae5bcb56e2f26bb13c83df5d4f2a0d18a73a43
Seu Nome <seu@email.com>
2025-09-08 21:17:14 -0300

    tf

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -7,7 +7,7 @@ def test_multiplica():
     assert multiplica(2, 3) == 6
 
 def test_divide():
-    assert potencia(4, 2) == 2
+    assert divide(4, 2) == 2
 
 def test_subtração():
     assert subtração(4, 1) == 3
